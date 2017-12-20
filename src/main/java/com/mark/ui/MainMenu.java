f9a62228983ae52ce284334aa68732af2deb519c
mark <maliqianglyg@163.com>
2017-12-20 23:21:28 +0800

    补充说明

---
+++ src/main/java/com/mark/ui/MainMenu.java
@@ -0,0 +1,15 @@
+/**
+ * @file MainMenu.java
+ * @project PDM2File
+ * @copyright 无锡雅座在线科技股份有限公司
+ */
+package com.mark.ui;
+
+/**
+ *
+ * @author maliqiang
+ * @create 2017-12-20
+ * @version 1.0
+ */
+public class MainMenu {
+}
